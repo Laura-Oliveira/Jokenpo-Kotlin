0f9cb0d96777852f8fe99660651660af5a9586f9
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-11-01 22:51:09 -0300

    [FrontEnd] Criacao do layout da tela de activity_player | Ajustes identidade visual

--- app/src/main/java/com/app/jokenpo/Controller/PlayerActivity.kt
+++ app/src/main/java/com/app/jokenpo/Controller/PlayerActivity.kt
@@ -7,6 +7,6 @@ import com.app.jokenpo.R
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_player)
     }
 }
