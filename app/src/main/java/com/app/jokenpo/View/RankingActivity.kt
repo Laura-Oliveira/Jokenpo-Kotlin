7aa677ed7807d0ddff98e5766380235d660a330c
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-11-02 16:05:39 -0300

    [FrontEnd] Ajustes layout no modelo do design no arquivo styles.xml | Ajustes layout de elementos nas telas activity_main, activity_player e activity_ranking

---
+++ app/src/main/java/com/app/jokenpo/View/RankingActivity.kt
@@ -0,0 +1,12 @@
+package com.app.jokenpo.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.jokenpo.R
+
+class RankingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ranking)
+    }
+}
