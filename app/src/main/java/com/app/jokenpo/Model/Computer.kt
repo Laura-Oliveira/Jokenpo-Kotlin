5542abeeaad652b5b1ab629f41a368f8ce4a5e23
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-11-04 20:56:35 -0300

    [BackEnd] Inclusao da entidade computer para buscar nomes de oponente na internet

---
+++ app/src/main/java/com/app/jokenpo/Model/Computer.kt
@@ -0,0 +1,11 @@
+package com.app.jokenpo.Model
+
+class Computer
+{
+    var computerName:String = " "
+        
+        get() = field
+        set(value) {
+            field = value
+        }
+}
